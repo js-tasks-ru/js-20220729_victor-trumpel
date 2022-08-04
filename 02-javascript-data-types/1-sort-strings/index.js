28814e5d6566bd6cb9cacab9a779a808fdb64c19
Victor <victor.trumpel@gmail.com>
2022-08-04 12:53:34 +0300

    sortString task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,23 +6,8 @@
  */
 
 export function sortStrings(arr, param = 'asc') {
-  const str = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФФХХЦцЧчШшЩщЪъЫыЬьЭэЮюЯяAaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-  const mapValue = ((map, str) => {
-    for (let i = 0; i < str.length; i++) { map.set(str[i], i) }
-    return map;
-  })(new Map(), str);
-
-  const factor = { 'asc': 1, 'desc': -1 }
+  const paramKeys = { 'asc': 1, 'desc': -1 }
   return [...arr].sort((strA, strB) => {
-    const minLength = strA.length < strB.length ? strA.length : strB.length;
-    let diff = 0;
-    for (let i = 0; i < minLength; i++) {
-      const aValue = mapValue.get(strA[i]);
-      const bValue = mapValue.get(strB[i]);
-
-      if (Number.isInteger(aValue) && Number.isInteger(bValue)) diff = aValue - bValue;
-      if (diff !== 0) return diff * factor[param];
-    }
-    return diff;
+    return strA.localeCompare(strB, "ru-en-u-kf-upper") * paramKeys[param]
   })
 }
