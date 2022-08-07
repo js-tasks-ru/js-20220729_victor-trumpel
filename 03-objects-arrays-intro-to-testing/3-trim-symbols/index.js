a1ed93ed45916aa0f687cf7eb3ed59d12c8aa668
Victor <victor.trumpel@gmail.com>
2022-08-07 12:33:51 +0300

    add trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,14 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  return [...string].reduce((accStr, currentStr) => {
+    const { str } = accStr;
+    if (str[str.length - 1] !== currentStr) accStr.counter = 0;
+    if (str[str.length - 1] === currentStr) accStr.counter += 1;
+    if (!(accStr.counter >= size)) accStr.str += currentStr
+    return accStr
+  }, {
+    str: '',
+    counter: 0
+  }).str
 }
